f55fd36dacd6c7df3867182df638fa97ed897349
Llimy1 <alsgur990104@gmail.com>
2024-01-08 18:20:06 +0900

    :sparkles: #2 - SpringBoot 기본 설정

---
+++ shopping-mall/src/test/java/org/project/shoppingmall/ShoppingMallApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.shoppingmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
