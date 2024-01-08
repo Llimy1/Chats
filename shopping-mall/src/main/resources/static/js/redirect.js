cf38999c530e8a51d90e433e8c1c0909e49a0cb9
Llimy1 <alsgur990104@gmail.com>
2024-01-08 18:21:19 +0900

    :sparkles: #2 - 리다이렉트 js 파일 생성

---
+++ shopping-mall/src/main/resources/static/js/redirect.js
@@ -0,0 +1,15 @@
+function redirectToLogin() {
+    window.location.href = "html/login.html";
+}
+
+function redirectToSignup() {
+    window.location.href = "html/signup.html";
+}
+
+function redirectToPasswordSearch() {
+    window.location.href = "html/passwordSearch.html";
+}
+
+function redirectToHome() {
+    window.location.href = "../index.html";
+}
